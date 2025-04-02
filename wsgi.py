b084fdab75d98215bf995e96291aea32c946a8c9
Joe Martucci <josephmartuccijr@live.com>
2025-04-02 10:59:20 -0400

    Reorganize project structure: Move health checks to core, consolidate config files, and improve service organization

--- wsgi.py
+++ wsgi.py
@@ -8,9 +8,9 @@ sys.path.append(str(current_dir))
 
 # Set environment variables
 os.environ["PYTHONPATH"] = str(current_dir)
-os.environ["APP_ENVIRONMENT"] = "production"
-os.environ["DEBUG"] = "true"
-os.environ["LOG_LEVEL"] = "debug"
+os.environ["APP_ENVIRONMENT"] = os.getenv("APP_ENVIRONMENT", "production")
+os.environ["DEBUG"] = os.getenv("DEBUG", "false")
+os.environ["LOG_LEVEL"] = os.getenv("LOG_LEVEL", "info")
 
 # Import and run the FastAPI app
 from app.main import app 
