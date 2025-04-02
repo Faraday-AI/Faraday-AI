c6048602c669816761f2158a3f33e828415b7b8f
Joe Martucci <josephmartuccijr@live.com>
2025-04-02 17:24:31 -0400

    Moved endpoint files from v1 to main endpoints directory

--- app/api/v1/endpoints/__init__.py
+++
@@ -1,9 +0,0 @@
-from .memory import router as memory_router
-from .math_assistant import router as math_assistant_router
-from .science_assistant import router as science_assistant_router
-
-__all__ = [
-    'memory_router',
-    'math_assistant_router',
-    'science_assistant_router'
-] 
